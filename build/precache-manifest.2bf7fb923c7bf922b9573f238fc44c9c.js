fc0cb9fb8bedac6bcee26382d92f6dc64fdfff34
UserPC <dm3iy.p@gmail.com>
2020-04-06 07:27:23 +0300

    исправил не значительные ошибки

---
+++ build/precache-manifest.2bf7fb923c7bf922b9573f238fc44c9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7981e59de888ca28dc419094f7402e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e50850ea62323ebdb2e0",
+    "url": "./static/css/main.c05e7f67.chunk.css"
+  },
+  {
+    "revision": "6cd2c68afe77d9fd37e6",
+    "url": "./static/js/2.7bc1a09f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7bc1a09f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50850ea62323ebdb2e0",
+    "url": "./static/js/main.13e1ca20.chunk.js"
+  },
+  {
+    "revision": "52f63c7561df7211f778",
+    "url": "./static/js/runtime-main.c510eba2.js"
+  }
+]);
