fa2ccb35f9bc0659eaed2bc5c0ae9b703ceaa067
deim0707 <dima1parshin@gmail.com>
2020-04-11 11:24:38 +0300

    убрал лишние картинки сделал возвращение в корневой каталог приложения, если набран не правильный путь (работает при BrowserRouter)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import './app.css';
 import Header from '../header';
@@ -10,17 +10,21 @@ import WelcomePage from "../sw-pages/welcome-page";
 const App = () =>  {
     return (
         <Router>
-            <div className='mx-3'>
-                <Header/>
-                <RandomPlanet updateInterval={undefined}/> {/*если пропс будет указан undefined, то используется значение по умолчанию */}
+            <Switch>
+                <div className='mx-3'>
+                    <Header/>
+                    <RandomPlanet updateInterval={undefined}/> {/*если пропс будет указан undefined, то используется значение по умолчанию */}
 
-                <Route path='/' component={WelcomePage} exact />  {/*exact - строгое указание того, что роутер должен искать не наличие пути на компонент. а строгое ему соответствие. написание exact === exact={true}*/}
+                    <Route path='/' component={WelcomePage} exact />  {/*exact - строгое указание того, что роутер должен искать не наличие пути на компонент. а строгое ему соответствие. написание exact === exact={true}*/}
 
-                <Route path="/people" render={()=><PersonPage typePerson='people'/>} />
-                <Route path="/planet" render={(props)=><PersonPage typePerson='planet' {...props}/> } /> {/*не обязательная вариация*/}
-                <Route path="/starship" exact render={()=><PersonPage typePerson='starship'/>} />
-                <Route path="/starship/:id" render={()=><PersonPage typePerson='starship'/>} /> {/*заготовка под отображение по определённому пути конкретной Персоны*/}
-            </div>
+                    <Route path="/people" render={()=><PersonPage typePerson='people'/>} />
+                    <Route path="/planet" render={(props)=><PersonPage typePerson='planet' {...props}/> } /> {/*не обязательная вариация*/}
+                    <Route path="/starship" exact render={()=><PersonPage typePerson='starship'/>} />
+                    <Route path="/starship/:id" render={()=><PersonPage typePerson='starship'/>} /> {/*заготовка под отображение по определённому пути конкретной Персоны*/}
+
+                    <Route render={()=><h2>Page not found</h2>}/> {/*Route без указания страницы, заключённый в Switch будет срабатывать, если указанная страница не найдена*/}
+                </div>
+            </Switch>
         </Router>
     );
 };
