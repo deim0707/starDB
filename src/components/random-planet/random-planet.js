e20e4f851e5f1b37b050c771f665b0a76c0a6ff5
deim0707 <dima1parshin@gmail.com>
2020-04-17 11:14:29 +0300

    добавил чуть-чуть адаптивности в хедере и рэндом плэнет

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -90,7 +90,7 @@ const PlanetView = ({planet}) => {
   const {id, name, population, rotationPeriod, diameter} = planet;
   return (
       <React.Fragment>
-        <img className="planet-image"
+        <img className="planet-image img-fluid"
              src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
              alt={name}
         />
@@ -98,15 +98,15 @@ const PlanetView = ({planet}) => {
           <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
-              <span className="term">Population</span>
+              <span className="term">Population:</span>
               <span>{population}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Rotation Period</span>
+              <span className="term">Rotation Period:</span>
               <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Diameter</span>
+              <span className="term">Diameter:</span>
               <span>{diameter}</span>
             </li>
           </ul>
